66fcd62f52845281aaf8b12856eb574afd800c0f
= <=>
2023-08-02 17:01:02 +0200

    Change navbar logo

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,8 +6,8 @@ import React from "react";
 
 const Navbar: React.FC = () => {
     return (
-        <nav className={'flex lg:hidden justify-between p-4 md:p-6 bg-semi-dark-blue md:mx-6 md:mt-6 md:rounded-[10px]'}>
-            <Image src={Logo} alt={'logo'} height={26} width={32} />
+        <nav className={'flex lg:hidden items-center justify-between p-4 md:p-6 bg-semi-dark-blue md:mx-6 md:mt-6 md:rounded-[10px]'}>
+            <Image src={Logo} alt={'logo'} height={32} width={32} className={'h-[20px] w-[25px] md:w-[32px] md:h-[25.6px]'}  />
             <MenuItems />
             <ProfileIcon/>
         </nav>
