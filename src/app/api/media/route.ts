89af4e1a0e7f55154f3800732b1b4b238f9a4913
= <=>
2023-08-05 17:53:23 +0200

    Hide fetchMedia function from route.ts file to avoid next.js build errors

--- src/app/api/media/route.ts
+++ src/app/api/media/route.ts
@@ -3,12 +3,12 @@ import {promises as fs} from 'fs';
 import {MediaEntity} from "@/app/api/media/types";
 
 // Server side data fetching method
-export async function fetchMedia(): Promise<MediaEntity[]> {
-    const jsonDirectory = path.join(process.cwd());
-    const fileContents = await fs.readFile(jsonDirectory + '/data.json', 'utf8');
-
-    return JSON.parse(fileContents);
-}
+// export async function fetchMedia(): Promise<MediaEntity[]> {
+//     const jsonDirectory = path.join(process.cwd());
+//     const fileContents = await fs.readFile(jsonDirectory + '/data.json', 'utf8');
+//
+//     return JSON.parse(fileContents);
+// }
 // Client side handler for fetching data
 export async function GET(req: Request){
     if(req.method !== 'GET'){
