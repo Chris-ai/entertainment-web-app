bdd9815ce48d05ad5a2ddd3f4ba27a9f71ed951f
chris-ai <krzysiek.kondracikowski@gmail.com>
2024-01-31 11:25:32 +0100

    Fix chaching problem

--- src/app/api/media/route.ts
+++ src/app/api/media/route.ts
@@ -1,22 +1,34 @@
-import path from 'path';
-import {promises as fs} from 'fs';
+import path from "path";
+import { promises as fs } from "fs";
+import { revalidatePath } from "next/cache";
 
-export async function GET(req: Request){
-    if(req.method !== 'GET'){
-        throw new Error('Method not allowed')
-    }
+export async function GET(req: Request) {
+  if (req.method !== "GET") {
+    throw new Error("Method not allowed");
+  }
 
-    const jsonDirectory = path.join(process.cwd());
-    const fileContents = await fs.readFile(jsonDirectory + '/data.json', 'utf8');
+  const jsonDirectory = path.join(process.cwd());
+  const fileContents = await fs.readFile(jsonDirectory + "/data.json", "utf8");
 
-    return new Response(fileContents)
+  return new Response(fileContents);
 }
 
 export async function PUT(req: Request) {
-    const jsonDirectory = path.join(process.cwd());
-    if(req.method !== 'PUT'){
-    throw new Error('Method not allowed')
-    }
-    const body = await req.json();
-    await fs.writeFile(jsonDirectory + '/data.json', Buffer.from(JSON.stringify(body)));
+  const jsonDirectory = path.join(process.cwd());
+  if (req.method !== "PUT") {
+    throw new Error("Method not allowed");
+  }
+  const body = await req.json();
+  revalidateCache();
+  await fs.writeFile(
+    jsonDirectory + "/data.json",
+    Buffer.from(JSON.stringify(body))
+  );
 }
+
+const revalidateCache = () => {
+  revalidatePath("/");
+  revalidatePath("/movies");
+  revalidatePath("/tv");
+  revalidatePath("/bookmarks");
+};
--- src/components/common/BookmarkButton.tsx
+++ src/components/common/BookmarkButton.tsx
@@ -5,38 +5,55 @@ import BookmarkEmpty from "@/assets/icons/icon-bookmark-empty.svg";
 import Button from "@/components/common/Button";
 
 interface IProps {
-    isBookmarked: boolean,
-    onClick: () => void
+  isBookmarked: boolean;
+  onClick: () => void;
 }
 
-const Bookmark: React.FC<{isBookmarked: boolean}> = ({isBookmarked}) => {
-    if(isBookmarked){
-        return (
-            <Image src={BookmarkFull} alt={"bookmark-full-button"} width={32} height={32} className={"h-4 w-4"} />
-        );
-    }
-    else {
-        return (
-            <Image src={BookmarkEmpty} alt={"bookmark-empty-button"} width={32} height={32} className={"h-4 w-4"}/>
-        );
-    }
-};
-
-
-const BookmarkButton: React.FC<IProps> = ({isBookmarked, onClick}) => {
-
-    const handleOnClick = () => {
-        onClick();
-    }
-
+const Bookmark: React.FC<{ isBookmarked: boolean }> = ({ isBookmarked }) => {
+  if (isBookmarked) {
     return (
-        <div className={'absolute right-2 top-2 bg-dark-blue bg-opacity-25 rounded-full'}>
-            <Button appearance={'transparent'} type={'icon'} onClick={handleOnClick} className={'p-2'}>
-                <Bookmark isBookmarked={isBookmarked} />
-            </Button>
-        </div>
+      <Image
+        src={BookmarkFull}
+        alt={"bookmark-full-button"}
+        width={32}
+        height={32}
+        className={"h-4 w-4"}
+      />
     );
+  } else {
+    return (
+      <Image
+        src={BookmarkEmpty}
+        alt={"bookmark-empty-button"}
+        width={32}
+        height={32}
+        className={"h-4 w-4"}
+      />
+    );
+  }
 };
 
+const BookmarkButton: React.FC<IProps> = ({ isBookmarked, onClick }) => {
+  const handleOnClick = () => {
+    onClick();
+  };
+
+  return (
+    <div
+      className={
+        "absolute right-2 top-2 bg-dark-blue bg-opacity-25 rounded-full"
+      }
+    >
+      <Button
+        appearance={"transparent"}
+        type={"icon"}
+        onClick={handleOnClick}
+        className={"p-2"}
+      >
+        <Bookmark isBookmarked={isBookmarked} />
+      </Button>
+    </div>
+  );
+};
 
 export default BookmarkButton;
